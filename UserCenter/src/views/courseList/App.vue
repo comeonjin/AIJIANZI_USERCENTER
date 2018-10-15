b4c2744ab3ab5a42af51ed23a97493b144216c6d
zhangjin <jincomeon@163.com>
2018-10-15 20:06:10 +0800

    add project

---
+++ UserCenter/src/views/courseList/App.vue
@@ -0,0 +1,30 @@
+<template>
+    <div id="app">
+        <!-- <img src="@/assets/logo.png"> -->
+        this is page couser_list
+        <Process />
+
+    </div>
+</template>
+
+<script>
+import Process from './components/Process';
+
+export default {
+    name: 'App',
+    components: {
+        Process,
+    },
+};
+</script>
+
+<style>
+#app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    /* margin-top: 60px; */
+}
+</style>
---
+++ UserCenter/src/views/courseList/components/Process.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="processContainer">
+        this is component process
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+
+        };
+    },
+};
+</script>
+
+<style lang="less" scoped>
+    .processContainer{
+        background: blue;
+    }
+</style>
